97741e21bbaf2d90649cda09158e57c0751d5532
Courtney <courtney.mae.phillips@gmail.com>
2015-06-12 10:22:32 -0700

    Have ability to delete questions.

--- controllers/question.js
+++ controllers/question.js
@@ -6,6 +6,12 @@ Forum.QuestionController = Ember.ObjectController.extend({
     },
     save: function() {
       this.set('isEditing', false);
+    },
+    delete: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions');
+      }
     }
   }
 });
